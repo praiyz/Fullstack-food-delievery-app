a600aec3df00f854e8f79e22e8555ff5fd50b881
olatunji praise <praiseolatunji22@gmail.com>
2025-02-03 08:10:34 -0800

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-const url = "https://fullstack-food-delievery-app.onrender.com";
+const url = "https://fullstack-food-delievery-app-1.onrender.com";
 
   return (
     <div>
