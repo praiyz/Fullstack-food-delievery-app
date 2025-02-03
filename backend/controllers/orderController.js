bb43d328984b4deabd1078a8dea6252c334b85e2
Olatunji praise <praiseolatunji22@gmail.com>
2025-02-03 14:03:56 -0800

    fullstack video 2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://frontend-mauve-seven-68.vercel.app";
+  const frontend_url = "https://frontend-buzn.onrender.com";
 
   try {
     const newOrder = new orderModel({
